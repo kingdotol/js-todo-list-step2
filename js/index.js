b5b5ceedc325133df4263e222e188872b25ba43f
kingdotol <oh@dotol.net>
2020-09-15 16:17:35 +0900

    user 불러오기

--- js/index.js
+++ js/index.js
@@ -5,3 +5,15 @@ const onUserCreateHandler = () => {
 const userCreateButton = document.querySelector('.user-create-button')
 userCreateButton.addEventListener('click', onUserCreateHandler)
 
+fetch('https://js-todo-list-9ca3a.df.r.appspot.com/api/users/')
+  .then(response => response.json())
+  .then(json => userLoad(json));
+
+function userLoad(users){
+  const userList = document.querySelector("#user-list");
+  const template = users.map(user => userTemplate(user.name));
+  userList.innerHTML = "\n"+template.join("\n")+"\n";
+}
+function userTemplate(name){
+  return `<button class="ripple">${name}</button>`
+}
